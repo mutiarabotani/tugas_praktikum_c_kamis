d6625018553c79875a9949e87f1742088fc5a6cb
unknown <mutiarabotani381@gmail.com>
2023-12-04 09:11:17 +0700

    first commit

---
+++ tugas mandiri 1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int nim = 2341027;
+	const char* nama = "Mutiara Botani";
+	const char* jurusan = "Teknik Informatika";
+	
+	printf("NIM : %d \n", nim);
+	printf("NAMA : %s \n", nama);
+	printf("JURUSAN: %s \n", jurusan);
+	
+	return 0;
+	
+}
